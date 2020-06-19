86f2513971477edea483271bfe079b2078a413ee
nghaosiong98 <nghaosiong98@gmail.com>
2020-06-19 10:39:16 +0800

    added add review functions

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -15,6 +15,8 @@ import Layout from "../components/layout"
 import img from "../images/item01.png"
 import Rating from "@material-ui/lab/Rating"
 import axios from "axios"
+import { useFormik } from "formik"
+import * as Yup from "yup"
 
 const useStyles = makeStyles({
   img: {
@@ -32,6 +34,33 @@ const IndexPage = () => {
   const [overallRate, setOverallRate] = useState(0.0)
   const [loading, setLoading] = useState(true)
 
+  const formik = useFormik({
+    validateOnBlur: false,
+    validateOnChange: false,
+    validationSchema: Yup.object().shape({
+      rating: Yup.number().min(1).max(5),
+      review: Yup.string().required(),
+    }),
+    initialValues: {
+      rating: 0,
+      review: "",
+    },
+    onSubmit: async (values, { resetForm }) => {
+      setLoading(true)
+      await axios
+        .post("http://127.0.0.1:5000/add_review", values)
+        .then(response => {
+          console.log(response)
+          updateReviews(prev => [...prev, values])
+          resetForm()
+          setLoading(false)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    },
+  })
+
   const fetchData = async () => {
     await axios
       .get("http://127.0.0.1:5000/reviews")
@@ -47,18 +76,18 @@ const IndexPage = () => {
       })
   }
 
-  const calcOverallRate = (reviews) => {
+  const calcOverallRate = reviews => {
     var total = 0.0
     reviews.forEach(element => {
-      total += (element.rating*1.0)
-    });
-    
-    return parseFloat(total/reviews.length).toFixed(1)
+      total += element.rating * 1.0
+    })
+    return parseFloat(total / reviews.length).toFixed(1)
   }
 
   useEffect(() => {
     fetchData()
   }, [])
+
   return (
     <Layout>
       <Grid container spacing={3}>
@@ -121,25 +150,32 @@ const IndexPage = () => {
                 <Grid item container spacing={2}>
                   <Grid item xs={12}>
                     <Rating
-                      name="simple-controlled"
-                      // value={value}
-                      // onChange={(event, newValue) => {
-                      //   setValue(newValue)
-                      // }}
+                      id="rating"
+                      name="rating"
+                      type="number"
+                      value={formik.values.rating}
+                      onChange={(_, value) => {
+                        formik.setFieldValue("rating", value)
+                      }}
                     />
                   </Grid>
                   <Grid item xs={9}>
                     <TextField
                       className={classes.text_input}
                       id="review"
+                      name="review"
                       label="Your Review"
                       multiline
                       rowsMax={5}
                       variant="outlined"
+                      value={formik.values.review}
+                      onChange={formik.handleChange}
                     />
                   </Grid>
                   <Grid item container xs={3} alignItems="center">
-                    <Button variant="contained">Submit</Button>
+                    <Button variant="contained" onClick={formik.handleSubmit}>
+                      Submit
+                    </Button>
                   </Grid>
                 </Grid>
                 <Grid item>
@@ -147,9 +183,8 @@ const IndexPage = () => {
                 </Grid>
                 <Grid item container direction="column" spacing={2}>
                   {reviews.map((v, i) => {
-                    console.log(v)
                     return (
-                      <Grid item>
+                      <Grid item key={i}>
                         <Rating value={v.rating} readOnly />
                         <Typography>{v.review}</Typography>
                       </Grid>
