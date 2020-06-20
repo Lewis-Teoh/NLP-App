e23e9deaac04515d1fc0e260f8046ffcc16502ca
Lewis97 <lewis@gzit.com.my>
2020-06-21 00:00:35 +0800

    Update integration with model

--- backend/app.py
+++ backend/app.py
@@ -1,40 +1,86 @@
+import datetime
+import numpy as np
+import tensorflow as tf
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-import datetime
 from flask_cors import CORS
+
+from tensorflow.keras.preprocessing.text import Tokenizer as KerasTokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from sentence_parser import Parser
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 CORS(app)
 
 db = SQLAlchemy(app)
+MODEL = tf.keras.models.load_model('cp.h5')
+
+def analyzerWrapper(review):
+    X_list = []
+    parser_obj = Parser(sent_segmenter=True)
+    parser_obj.sent_to_doc(review)
+    parsed_list = parser_obj.remove_punct()
+    str_ = ""
+    for idx_, list_ in enumerate(parsed_list):
+        str_ += ' '.join(list_)
+        str_ += ' '
+    X_list.append(str_)
+    
+    X_copy = X_list.copy() 
+    tk = KerasTokenizer(lower = True)
+    tk.fit_on_texts(X_copy)
+    X_seq = tk.texts_to_sequences(X_copy)
+    X_pad = pad_sequences(X_seq, maxlen=200, padding='post')
+
+    pred = MODEL.predict(X_pad, verbose=1)
+    y_pred = np.argmax(pred,axis=-1)[0] # 0=pos , 1=neu, 2=neg according to training model format
+    mapping = {
+        0:'pos',
+        1:'neu',
+        2:'neg',
+    }
+    label = mapping[y_pred]
+    return label
 
 class Review(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  rating = db.Column(db.Integer)
-  review = db.Column(db.String(200))
-  created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    review = db.Column(db.String(200))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    model_sentiment = db.Column(db.String(10))
+
 
 @app.route('/add_review', methods=['POST'])
 def add_review():
-  review_data = request.get_json()
+    review_data = request.get_json()
+    print(type(review_data['review']))
+    try:
+        label = analyzerWrapper(review_data['review'])
+    except Exception as e:
+        print(f"Failed to do prediction > {str(e)}")
+        label = None
 
-  new_review = Review(rating=review_data['rating'], review=review_data['review'])
+    new_review = Review(rating=review_data['rating'], review=review_data['review'], model_sentiment=label)
 
-  db.session.add(new_review)
-  db.session.commit()
+    db.session.add(new_review)
+    db.session.commit()
 
-  return 'Done', 201
+    return 'Done', 201
+
 
 @app.route('/reviews')
 def reviews():
-  review_list = Review.query.all()
-  reviews = []
+    review_list = Review.query.all()
+    reviews = []
 
-  for review in review_list:
-    reviews.append({'rating':review.rating, 'review':review.review})
+    for review in review_list:
+        reviews.append({'rating': review.rating, 'review': review.review})
 
-  return jsonify({'reviews': reviews})
+    return jsonify({'reviews': reviews})
 
-if __name__=='__main__':
-  app.run(debug=True)
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True, threaded=False)
---
+++ backend/sentence_parser.py
@@ -0,0 +1,78 @@
+import re
+import spacy
+import keras
+import numpy as np
+
+from spacy.tokenizer import Tokenizer 
+from spacy.util import compile_prefix_regex, compile_infix_regex, compile_suffix_regex
+from spacy.attrs import ORTH, NORM
+
+special_case = {
+    "couldn't" : [{ORTH: "could"}, {ORTH: "n't", NORM: "not"}],
+    "don't" : [{ORTH: "do"}, {ORTH: "n't", NORM: "not"}],
+    "wasn't" : [{ORTH: "was"}, {ORTH: "n't", NORM: "not"}],
+    "isn't" : [{ORTH: "is"}, {ORTH: "n't", NORM: "not"}],
+    "it's" : [{ORTH: "it"}, {ORTH: "'s", NORM: "is"}],
+    "wouldn't": [{ORTH: "would"}, {ORTH: "n't", NORM: "not"}],
+}
+
+class Parser:
+    def __init__(self, sent_segmenter=True, custom_tokenize=True):
+        self.pre_model = spacy.load("en_core_web_sm")
+        
+        if sent_segmenter:
+            self.boundary = re.compile('^[0-9]$')
+            self.pre_model.add_pipe(self.custom_seg, before='parser')
+        
+        if custom_tokenize:
+            self.pre_model.tokenizer = self.custom_tokenizer(self.pre_model)
+            
+            for k,v in special_case.items():
+                self.pre_model.tokenizer.add_special_case(k, v)
+            
+        print(f"Pipe names : {self.pre_model.pipe_names}")
+    
+    def custom_tokenizer(self, nlp_model):
+        infix_re = re.compile(r'''[.\,\?\:\;\...\‘\’\`\“\”\"\'~]''')
+        prefix_re = compile_prefix_regex(self.pre_model.Defaults.prefixes)
+        suffix_re = compile_suffix_regex(self.pre_model.Defaults.suffixes)
+        
+        return Tokenizer(self.pre_model.vocab, prefix_search=prefix_re.search,
+                                               suffix_search=suffix_re.search,
+                                               infix_finditer=infix_re.finditer,
+                                               token_match=None)
+
+    def custom_seg(self, doc):
+        prev = doc[0].text
+        length = len(doc)
+        for index, token in enumerate(doc):
+            if (token.text == '.' and self.boundary.match(prev) and index!=(length - 1)):
+                doc[index+1].sent_start = False
+            prev = token.text
+        return doc
+    
+    def sent_to_doc(self, sent):
+        self.doc = self.pre_model(sent)
+    
+    def token_normalizer(self, token):
+        if(token == "n't"):
+            new_token = "not"
+        else:
+            new_token = token
+        return new_token
+    
+    def remove_punct(self):
+        if 'custom_seg' in self.pre_model.pipe_names:
+            rlist = []
+            for sent in (self.doc).sents:
+                temp_ = []
+                for token in sent:
+                    if token.pos_ == 'PUNCT':
+                        continue
+                    new_token = self.token_normalizer(token.text)
+                    temp_.append(new_token)
+                rlist.append(temp_)
+            return rlist
+
+# if __name__ == "__main__":
+#     pass
